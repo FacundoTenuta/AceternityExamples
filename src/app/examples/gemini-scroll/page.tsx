2d344df84fb0659b91d3d25ab9604b0982676220
Facundo Tenuta <facu_201117@hotmail.com>
2024-02-28 21:29:32 -0300

    Update page.tsx with Gemini Scroll example

---
+++ src/app/examples/gemini-scroll/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useRef } from 'react';
+import { useScroll, useTransform } from 'framer-motion';
+import { GoogleGeminiEffect } from '@/components/geminiScrollEffect/GoogleGeminiEffect';
+
+const page = () => {
+	const ref = useRef(null);
+
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ['start start', 'end start'],
+	});
+
+	const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+	const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+	const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+	const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+	const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
+	return (
+		<div className="h-[3000px] mt-60 flex justify-center items-center">
+			<GoogleGeminiEffect
+				className="h-[3000px]"
+				pathLengths={[
+					pathLengthFirst,
+					pathLengthSecond,
+					pathLengthThird,
+					pathLengthFourth,
+					pathLengthFifth,
+				]}
+			/>
+		</div>
+	);
+};
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<>
 			<h1 className="text-white font-bold text-4xl">Examples</h1>
 
-			<div className="flex flex-col items-center mt-4">
+			<div className="flex flex-col gap-2 items-center mt-4">
 				<Link
 					href="/examples/macbook-scroll"
 					className="bg-slate-500 text-white px-4 py-2 rounded-md transition-all duration-300 hover:bg-slate-600"
@@ -14,7 +14,7 @@ export default function Home() {
 				</Link>
 				<Link
 					href="/examples/gemini-scroll"
-					className="bg-red-600 text-white px-4 py-2 rounded-md mt-4"
+					className="bg-slate-500 text-white px-4 py-2 rounded-md transition-all duration-300 hover:bg-slate-600"
 				>
 					Gemini Scroll
 				</Link>
