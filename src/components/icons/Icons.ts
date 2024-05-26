bd106d4e959c1f60e05ed0b0c533faf83b85b888
Facundo Tenuta <facu_201117@hotmail.com>
2024-05-26 13:19:47 -0300

    feat: Add new icons and icon component

---
+++ src/components/icons/Icons.ts
@@ -0,0 +1,21 @@
+import { Github } from './Github';
+import { GoogleDocs } from './GoogleDocs';
+import { GoogleDrive } from './GoogleDrive';
+import { Messenger } from './Messenger';
+import { Notion } from './Notion';
+import { OpenAi } from './OpenAi';
+import { User } from './User';
+import { WhatsApp } from './WhatsApp';
+import { Zapier } from './Zapier';
+
+export const Icons = {
+    User,
+    GoogleDocs,
+    GoogleDrive,
+    Notion,
+    WhatsApp,
+    Github,
+    Messenger,
+    OpenAi,
+    Zapier,
+};
